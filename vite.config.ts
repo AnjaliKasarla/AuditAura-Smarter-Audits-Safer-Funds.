91386dddd49cf327ec7cdab2eb843f02d0b5e2ec
AnjaliKasarla <2420030170@klh.edu.in>
2025-12-26 19:49:09 +0530

    Added all files to repo

---
+++ vite.config.ts
@@ -0,0 +1,20 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import path from "path";
+import { componentTagger } from "lovable-tagger";
+
+export default defineConfig(({ mode }) => ({
+  base: "/", // ✅ REQUIRED for Nginx + Docker
+
+  plugins: [
+    react(),
+    mode === "development" && componentTagger(),
+  ].filter(Boolean),
+
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+}));
+
